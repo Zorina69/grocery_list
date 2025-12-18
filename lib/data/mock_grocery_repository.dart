39844733b838a56844f32cb4d630111dbdcd1a7b
Zorina69 <soychanratana14@gmail.com>
2025-12-18 09:08:42 +0700

    commit

---
+++ lib/data/mock_grocery_repository.dart
@@ -0,0 +1,16 @@
+import '../models/grocery.dart';
+
+final dummyGroceryItems = [
+  Grocery(
+      name: 'Milk',
+      quantity: 1,
+      category: GroceryCategory.dairy),
+  Grocery(
+      name: 'Bananas',
+      quantity: 5,
+      category: GroceryCategory.fruit),
+  Grocery(
+      name: 'Beef Steak',
+      quantity: 1,
+      category: GroceryCategory.meat),
+];
